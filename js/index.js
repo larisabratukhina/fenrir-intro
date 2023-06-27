677f3787dfba7c81a1a1c63922298b8586cc9272
Lara Bee <larisa.bratukhina@gmail.com>
2023-06-26 20:48:28 -0700

    Create Stylesheet_Write CSS_Add Image

--- js/index.js
+++ js/index.js
@@ -1,29 +1,19 @@
-// Get the current date
+/*
+Get the current date, year, footer element, create a new paragraph element for the copyright text
+Set the inner HTML of the copyright element to display my name and the current year
+Unicode to insert a copyright symbol in front of the year('&#169;' or'&#xA9;' or '&copy;')
+Append the copyright element to the footer
+*/
 const today = new Date();
-
-// Get the current year
 const thisYear = today.getFullYear();
-
-// Get the footer element
 const footer = document.querySelector('footer');
-
-// Create a new paragraph element for the copyright text
 const copyright = document.createElement('p');
-
-// Set the inner HTML of the copyright element to display your name and the current year
-// Unicode to insert a copyright symbol in front of the year('&#169;' or'&#xA9;' or '&copy;')
 copyright.innerHTML = 'Larysa Bratukhina ' + '&#xA9; ' + thisYear;
-
-// Append the copyright element to the footer
 footer.appendChild(copyright);
 
-// List my technical skills
+// Add my skills to the skills sections
 const skills = ['JavaScript', 'Java', 'CSS', 'HTML'];
-
-// Get the skills section
 const skillsSection = document.getElementById('skills');
-
-// Get the unordered list element
 const skillsList = skillsSection.querySelector('ul');
 
 // Loop through the skills array and add each skill to the list
@@ -62,7 +52,8 @@ messageForm.addEventListener("submit", (event) => {
     const messageSection = document.getElementById("messages");
     const messageList = messageSection.querySelector("ul");
     const newMessage = document.createElement("li");
-    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName} </a><span>wrote: &nbsp${usersMessage} </span>`;
+    newMessage.className = "message-section";
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}" class="message-email">${usersName} </a><span>wrote: &nbsp${usersMessage} </span>`;
 
     const removeButton = document.createElement("button");
     removeButton.innerText = "remove";
